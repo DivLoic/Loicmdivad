0ea8cdf7f0f8fc3c6460fdff3fedc8ae4207c308
Loïc <ldivad@isep.fr>
2016-03-04 21:26:26 +0100

    first deploy of march

---
+++ app/components/demo-card.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  demo: null,
+
+  actions:{
+    loadSource: function(){
+      return false;
+    }
+  }
+});
--- app/views/application.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.View.extend({
-  didInsertElement: function(){
-    $("#flipcard").flip();
-    $("nav p").css('padding-top', '190px');
-  }
-});
