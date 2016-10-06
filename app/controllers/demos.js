10bf652b5647bf43864aa700b3144e543e308a24
Loïc <ldivad@isep.fr>
2016-10-07 00:42:21 +0200

    add a legend to the demos page

--- app/controllers/demos.js
+++ app/controllers/demos.js
@@ -3,6 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   title: "Demos",
   type: "type",
+
+  legends: [
+    {'t': 'graph', 'img': 'graphdemo', 'color': '6eafd4', 'msg': 'graph demos'},
+    {'t': 'code', 'img': 'codedemo', 'color': '3683bb', 'msg': 'code demos'},
+    {'t': 'ml', 'img': 'mldemo', 'color': 'e45621', 'msg': 'machine learning demos'},
+    {'t': 'db', 'img': 'dbdemo', 'color': 'fb8d46', 'msg': 'database demos'}
+  ],
+
   records: Ember.computed.oneWay('model'),
 
   actions: {
--- app/models/demo.js
+++ app/models/demo.js
@@ -4,16 +4,16 @@ import Ember from 'ember';
 export default DS.Model.extend({
 
   title: DS.attr('string'),
-  description: DS.attr('string'),
   type: DS.attr('string'),
+  tools: DS.attr(),
+  seeAlso: DS.attr(),
+  credits: DS.attr(),
+  source: DS.attr('string'),
+  description: DS.attr('string'),
   date: DS.attr('date'),
   date_str: Ember.computed('date', function() {
     let d = this.get('date');
     return d.toISOString().substr(0,10);
-  }),
-  source: DS.attr('string'),
-  tools: DS.attr(),
-  seeAlso: DS.attr(),
-  credits: DS.attr()
+  })
 
 });
