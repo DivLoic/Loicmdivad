3aa3d9cfd68209b5f073dda032a2b641b04fbace
Loïc <ldivad@isep.fr>
2016-10-13 09:27:52 +0200

    :page_facing_up: add the pdf resume

--- app/router.js
+++ app/router.js
@@ -7,15 +7,17 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('me', {path: '/me'});
   this.route('home', {path: '/'});
   this.route('projects', {path: '/projects'});
-  this.route('me', {path: '/me'});
   this.route('player', {path: 'player/:id_demo'});
   this.resource('demos', {path: '/demos'}, function(){
     this.route('demostypes', {path: '/type/:type'});
   });
   this.route('demos');
   this.route('posts');
+  //TODO: add PDF.js to display the resume
+  //this.route('resume');
 });
 
 export default Router;
